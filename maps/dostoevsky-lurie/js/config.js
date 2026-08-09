3e2f906908b046e5014c477f0c65578e5b3565ea
Guzaliya23 <guzaliya@users.noreply.github.com>
2026-08-09 22:59:51 +0300

    Add Dostoevsky/Lurie and Piter FM maps; fix Brat Priozersk pin

---
+++ maps/dostoevsky-lurie/js/config.js
@@ -0,0 +1,6 @@
+// Ключ Яндекс.Карт (JavaScript API). В кабинете укажи домен GitHub Pages + localhost.
+// https://developer.tech.yandex.ru/
+window.WTL_CONFIG = {
+  yandexApiKey: "012f7cb8-8deb-44a8-9b3e-751149bc6644",
+  lang: "ru_RU"
+};
